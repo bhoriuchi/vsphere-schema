5cbc1bc069cad2b13a3d55eadc5d1449c9415bf0
Branden Horiuchi <bhoriuchi@gmail.com>
2015-12-27 15:58:21 -0800

    5.1 property schema

--- example/test.js
+++ example/test.js
@@ -1,4 +1,8 @@
 var schema = require('../lib');
+var _      = schema.env.lodash;
 
 
-console.log(JSON.stringify(schema['5.1'], null, '  '));
+
+console.log(JSON.stringify(schema['5.1'].VirtualMachine.properties.guest.properties.disk.properties, null, '  '));
+
+console.log(schema.getProperty('5.1', 'VirtualMachine', 'guest.disk'));
