a3b308bf486aeb04c67f1b77a427abede209a694
Branden Horiuchi <bhoriuchi@gmail.com>
2016-02-08 09:27:02 -0800

    fixing schema order

--- example/test.js
+++ example/test.js
@@ -1,8 +1,11 @@
 var schema = require('../lib');
 var _      = schema.env.lodash;
 
+var vm = schema['5.1'].VirtualMachine.properties;
 
 
-console.log(JSON.stringify(schema['5.1'].VirtualMachine.properties.guest.properties.disk.properties, null, '  '));
+//console.log(vm.config);
 
-console.log(schema.getProperty('5.1', 'VirtualMachine', 'guest.disk'));
+//console.log(JSON.stringify(schema['5.1'].VirtualMachine.properties, null, '  '));
+
+console.log(schema.getProperty('5.1', 'VirtualMachine', 'config'));
