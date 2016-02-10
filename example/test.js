b44cc4ac72834b8f94bd5cab3868607a34c4979a
Branden Horiuchi <bhoriuchi@gmail.com>
2016-02-10 10:00:22 -0800

    adding methods to docscrape

--- example/test.js
+++ example/test.js
@@ -1,11 +1,20 @@
 var schema = require('../lib');
 var _      = schema.env.lodash;
+var docUtil = require('../lib/scrape/docUtil');
 
-var vm = schema['5.1'].VirtualMachine.properties;
-
-
+//var vm = schema['5.1'].VirtualMachine.properties;
 //console.log(vm.config);
+//console.log(JSON.stringify(schema.env.versions['dev'].VirtualMachine, null, '  '));
+//console.log(schema.getProperty('10.0', 'Action', 'dynamicProperty'));
 
-//console.log(JSON.stringify(schema['5.1'].VirtualMachine.properties, null, '  '));
+var _self     = this;
+_self.schema  = {};
+_self.types   = {};
+var ver       = '2.5';
+var base      = __dirname.replace('/example', '/lib/scrape') + '/docs/' + ver + '/ReferenceGuide/';
+var doc       = 'vim.ManagedEntity.html';
 
-console.log(schema.getProperty('6.0', 'VirtualMachine', 'config'));
+
+docUtil.getDoc(doc, base, _self).then(function() {
+	console.log(_self.schema.ManagedEntity.methods);
+});
