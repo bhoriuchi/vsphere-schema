daf712b46648614531c78870f8b1bfc9772b129a
Branden Horiuchi <bhoriuchi@gmail.com>
2016-02-10 12:38:20 -0800

    updated schemas to include methods and use merge function
    * schemas now include methods and parameters
    * to minimize source code added merge function to extend objects
    * updated doc scrape to better order the dependencies

--- example/test.js
+++ example/test.js
@@ -4,17 +4,21 @@ var docUtil = require('../lib/scrape/docUtil');
 
 //var vm = schema['5.1'].VirtualMachine.properties;
 //console.log(vm.config);
-//console.log(JSON.stringify(schema.env.versions['dev'].VirtualMachine, null, '  '));
-//console.log(schema.getProperty('10.0', 'Action', 'dynamicProperty'));
-
-var _self     = this;
-_self.schema  = {};
-_self.types   = {};
-var ver       = '2.5';
-var base      = __dirname.replace('/example', '/lib/scrape') + '/docs/' + ver + '/ReferenceGuide/';
-var doc       = 'vim.ManagedEntity.html';
+console.log(JSON.stringify(schema.env.versions['6.0'].VirtualMachine, null, '  '));
+//console.log(schema.getProperty('dev', 'VirtualMachine', 'config'));
 
 
-docUtil.getDoc(doc, base, _self).then(function() {
-	console.log(_self.schema.ManagedEntity.methods);
-});
+var testDoc = function() {
+	var _self     = this;
+	_self.schema  = {};
+	_self.types   = {};
+	var ver       = '2.5';
+	var base      = __dirname.replace('/example', '/lib/scrape') + '/docs/' + ver + '/ReferenceGuide/';
+	var doc       = 'vim.DiagnosticManager.html';
+
+
+	docUtil.getDoc(doc, base, _self).then(function() {
+		console.log(_self.schema.DiagnosticManager.methods);
+	});	
+};
+
