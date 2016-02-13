ef91d801703824cab33ef625a49ff04adcabd9ab
Branden Horiuchi <bhoriuchi@gmail.com>
2016-02-12 16:02:31 -0800

    adding functions

--- example/test.js
+++ example/test.js
@@ -3,7 +3,8 @@ var _      = schema.env.lodash;
 
 //var vm = schema['5.1'].VirtualMachine.properties;
 //console.log(vm.config);
-console.log(JSON.stringify(schema.env.versions['6.0'].VirtualMachine, null, '  '));
+//console.log(JSON.stringify(schema.env.versions['6.0'].VirtualMachine, null, '  '));
 //console.log(schema.getProperty('dev', 'VirtualMachine', 'config'));
+console.log(schema.utils.getEventTypes('2.5'));
 
 
